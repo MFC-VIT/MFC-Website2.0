a6feab753fc706a85f707556b15b099428808588
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2024-10-22 11:20:29 +0530

    chores: improved-code-structure

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -7,6 +7,16 @@ import linkedin from '../assets/images/Vector-6.svg';
 import twitter from '../assets/images/Vector-7.svg';
 import medium from '../assets/images/Vector-1.svg'
 
+const socials = [
+  { src: facebook, href: "https://www.facebook.com/mfcvit/" },
+  { src: instagram, href: "https://www.instagram.com/mfc_vit/" },
+  { src: youtube, href: "https://www.youtube.com/c/MozillaFirefoxClubVIT" },
+  { src: github, href: "https://github.com/MFC-VIT" },
+  { src: linkedin, href: "https://www.linkedin.com/company/mfcvit/mycompany/" },
+  { src: twitter, href: "https://x.com/mfc_vit" },
+  { src: medium, href: "https://medium.com/mozilla-firefox-club/tagged/mfcvit" },
+]
+
 const useDynamicText = (id)=>{
   useEffect(()=>{
     const textElement = document.getElementById(id);
@@ -42,27 +52,11 @@ export const Footer = () => {
   return (
       <div className='relative w-full h-full'>
         <div className='flex justify-center space-x-2 py-4 absolute -top-2 max-lg:-top-8 max-sm:-top-10 left-0 right-0 mt-2'>
-          <a href='https://www.facebook.com/mfcvit/' target='_blank' className='h-fit'>
-            <img src={facebook} className='md:size-8 size-6 transition-all duration-200 hover:scale-150 max-md:hover:scale-125 hover:mx-2 max-md:hover:mx-1 active:scale-100' />
-          </a>
-          <a href='https://www.instagram.com/mfc_vit/' target='_blank' className='h-fit'>
-            <img src={instagram} className='md:size-8 size-6 transition-all duration-200 hover:scale-150 max-md:hover:scale-125 hover:mx-2 max-md:hover:mx-1 active:scale-100' />
-          </a>
-          <a href='https://www.youtube.com/c/MozillaFirefoxClubVIT' target='_blank' className='h-fit'>
-            <img src={youtube} className='md:size-8 size-6 transition-all duration-200 hover:scale-150 max-md:hover:scale-125 hover:mx-2 max-md:hover:mx-1 active:scale-100' />
-          </a>
-          <a href='https://github.com/MFC-VIT' target='_blank' className='h-fit'>
-            <img src={github} className='md:size-8 size-6 transition-all duration-200 hover:scale-150 max-md:hover:scale-125 hover:mx-2 max-md:hover:mx-1 active:scale-100' />
-          </a>
-          <a href='https://www.linkedin.com/company/mfcvit/mycompany/' target='_blank' className='h-fit'>
-            <img src={linkedin} className='md:size-8 size-6 transition-all duration-200 hover:scale-150 max-md:hover:scale-125 hover:mx-2 max-md:hover:mx-1 active:scale-100' />
-          </a>
-          <a href='https://x.com/mfc_vit' target='_blank' className='h-fit'>
-            <img src={twitter} className='md:size-8 size-6 transition-all duration-200 hover:scale-150 max-md:hover:scale-125 hover:mx-2 max-md:hover:mx-1 active:scale-100' />
-          </a>
-          <a href='https://medium.com/mozilla-firefox-club/tagged/mfcvit' target='_blank' className='h-fit'>
-            <img src={medium} className='md:size-8 size-6 transition-all duration-200 hover:scale-150 max-md:hover:scale-125 hover:mx-2 max-md:hover:mx-1 active:scale-100' />
-          </a>
+          {socials.map((social, index)=>(
+            <a href={social.href} target='_blank' className='h-fit' key={index}>
+              <img src={social.src} className='md:size-8 size-6 transition-all duration-200 hover:scale-150 max-md:hover:scale-125 hover:mx-2 max-md:hover:mx-1 active:scale-100' />
+            </a>
+          ))}
         </div>
         <div className='text-center text-[22vw] p-0 m-0 leading-none font-apex text-[#E8DED5]' id="firefox">
           FIREFOX
