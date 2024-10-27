70eacbf49f1400f0115a9bf947ec6e0c4d18d306
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2024-10-27 12:04:41 +0530

    add: events-page

---
+++ src/constants/events.js
@@ -0,0 +1,30 @@
+export const events = [
+  {
+    title: "TechWars",
+    description: "Compete in a thrilling campus-wide WAR, where your team’s strategy is the key to victory. Solve coding questions and conquer territories, play mini-games that offer offline power-ups, sharpening your skills while keeping the fun factor high.",
+    doe: "28/09/24",
+    image: "/src/assets/images/techwars.webp",
+    link: "https://github.com/MFC-VIT/TechWars-Frontend"
+  },
+  {
+    title: "TechWars",
+    description: "Compete in a thrilling campus-wide WAR, where your team’s strategy is the key to victory. Solve coding questions and conquer territories, play mini-games that offer offline power-ups, sharpening your skills while keeping the fun factor high.",
+    doe: "28/09/24",
+    image: "/src/assets/images/techwars.webp",
+    link: "https://github.com/MFC-VIT/TechWars-Frontend"
+  },
+  {
+    title: "TechWars",
+    description: "Compete in a thrilling campus-wide WAR, where your team’s strategy is the key to victory. Solve coding questions and conquer territories, play mini-games that offer offline power-ups, sharpening your skills while keeping the fun factor high.",
+    doe: "28/09/24",
+    image: "/src/assets/images/techwars.webp",
+    link: "https://github.com/MFC-VIT/TechWars-Frontend"
+  },
+  {
+    title: "TechWars",
+    description: "Compete in a thrilling campus-wide WAR, where your team’s strategy is the key to victory. Solve coding questions and conquer territories, play mini-games that offer offline power-ups, sharpening your skills while keeping the fun factor high.",
+    doe: "28/09/24",
+    image: "/src/assets/images/techwars.webp",
+    link: "https://github.com/MFC-VIT/TechWars-Frontend"
+  }
+]
