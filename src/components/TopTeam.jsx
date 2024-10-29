cd25e08f4f535a21ae0b4072745645aa7e3e6b94
Suvidh Jain <139179313+SuvidhJ@users.noreply.github.com>
2024-10-29 16:01:56 +0530

    Update TopTeam.jsx
    fix : faculty coordinator section

--- src/components/TopTeam.jsx
+++ src/components/TopTeam.jsx
@@ -4,41 +4,38 @@ const Toplayout = () => {
   return (
     <>
       <div className="text-[#E8DED5]">
-        {/* for the top part */}
         <div className="flex flex-col items-center">
-          <h1 className="text-center font-apex font-normal leading-12 md:text-6xl lg:text-8xl">
-            MEET <br></br> OUR TEAM
+          <h1 className="text-center font-apex font-normal leading-12 md:text-5xl lg:text-6xl">
+            MEET <br /> OUR TEAM
           </h1>
 
-          <div className="flex justify-center items-center mt-10 md:mt-20 lg:mt-32">
+          <div className="flex justify-center items-center mt-5 md:mt-10 lg:mt-16">
             <img
-              className="max-w-full h-auto object-cover rounded-md shadow-md filter grayscale(50%)"
+              className="max-w-[300px] h-auto object-cover rounded-full shadow-md filter grayscale(50%)" // Smaller size and rounded corners
               src={images}
               alt="Team Member Image"
             />
           </div>
 
-          <div className="flex flex-col items-center mt-5 md:mt-10 lg:mt-16">
-            <h1 className="font-apex text-2xl md:text-3xl lg:text-4xl">
+          <div className="flex flex-col items-center mt-3 md:mt-5 lg:mt-8">
+            <h1 className="font-apex text-xl md:text-2xl lg:text-3xl">
               MR. JAYAKUMAR S
             </h1>
-            <h1 className="text-orange-500 font-CAYoshiroTRIAL text-xl md:text-2xl lg:text-3xl">
+            <h1 className="text-orange-500 font-CAYoshiroTRIAL text-lg md:text-xl lg:text-2xl">
               Faculty Coordinator
             </h1>
           </div>
 
-          <div className="mt-5 md:mt-10 lg:mt-16 text-center font-CAYoshiroTRIAL text-sm md:text-base lg:text-lg">
+          <div className="mt-3 md:mt-4 lg:mt-5 text-center font-CAYoshiroTRIAL text-sm md:text-base lg:text-lg">
             <p>
               Jayakumar Sadhasivam is an Assistant Professor Senior Grade 1 at
-              Vellore Institute of Technology (VIT) in <br></br>the School of
+              Vellore Institute of Technology (VIT) in <br /> the School of
               Computer Science and Engineering (SCOPE). Jayakumar's areas of
-              expertise include Open <br></br> Source Programming, Network
+              expertise include Open <br /> Source Programming, Network
               Security, Storage Technologies, and Machine Learning. His research{" "}
-              <br></br> interests are in the use of technology in education and
-              developing open-source software that takes into <br></br>{" "}
-              <div className="text-center">
-                consideration the unique needs of learners
-              </div>
+              <br /> interests are in the use of technology in education and
+              developing open-source software that takes into <br /> consideration
+              the unique needs of learners. <br />
             </p>
           </div>
         </div>
@@ -46,4 +43,5 @@ const Toplayout = () => {
     </>
   );
 };
+
 export default Toplayout;
