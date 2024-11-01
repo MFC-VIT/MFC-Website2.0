64f5b0a41767bd8402a50d4ce1694ab6ef7bff09
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2024-11-01 16:00:07 +0530

    chores: fix mobile-responsivenss for event-card

--- src/sections/Event.jsx
+++ src/sections/Event.jsx
@@ -4,16 +4,16 @@ import { events } from "../constants/events";
 
 const Events = () => {
   return (  
-    <div className="flex flex-col items-center">
-      <div className="text-main text-6xl font-apex">Events at MFC</div>
-      <div className="grid grid-cols-2 grid-rows-2 gap-10 p-10 max-lg:flex max-lg:flex-col">
+    <div className="flex flex-col items-center" id="events">
+      <div className="text-main text-6xl max-md:text-5xl max-sm:text-4xl font-apex">Events at MFC</div>
+      <div className="grid grid-cols-2 grid-rows-2 gap-10 max-md:gap-6 p-10 max-md:p-5 max-sm:px-3 max-lg:flex max-lg:flex-col">
         {events.slice(0, 4).map((event, index)=>{
           return (
-            <Event {...events[index]} key={index} />
+            <Event {...events[index]} key={index} link={events[index].link || '#events'} />
           )
         })}
       </div>
-      <Link text="See More" />
+      <SeeMore link={"#events"} />
     </div>
   );
 }
@@ -22,14 +22,14 @@ const Event = ({
   title, description, doe, image, link
 }) => {
   return (
-    <div className="flex justify-between border-[3px] border-primary rounded-lg p-4 gap-5 bg-black">
-      <div className="flex justify-center items-center"><img src={image} /></div>
-      <div className="text-white gap-1 flex flex-col">
-        <div className="font-yoshiro text-primary font-bold text-xl">{title}</div>
-        <div className="font-yoshiro text-main">{description}</div>
-        <div className="font-yoshiro text-secondary">{doe}</div>
+    <div className="flex max-sm:flex-col justify-between border-[3px] border-primary rounded-2xl p-4 gap-5 max-sm:gap-1 bg-black">
+      <div className="flex justify-center items-center"><img src={image} className="min-h-36 min-w-36 max-sm:max-h-14 max-sm:max-w-14" /></div>
+      <div className="text-white gap-1 max-sm:gap-0 flex flex-col">
+        <div className="font-apex mb-1 tracking-wider text-primary font-bold text-3xl max-md:text-2xl">{title}</div>
+        <div className="font-yoshiro text-main text-lg max-md:text-base tracking-wide text-ellipsis mb-1">{description}</div>
+        <div className="font-yoshiro text-orange-500">{doe}</div>
         <div className="w-full flex justify-end">
-        <Link link={link} text="Explore" />
+        <Link link={link} text="Explore" textClass={"text-lg"} />
         </div>
       </div>
     </div>
@@ -45,13 +45,13 @@ Event.propTypes = {
 }
 
 const Link = ({link, text})=>{
+  
   return (
     <a
       href={link}
-      target="_blank"
-      className="w-fit bg-[#0A0807] border-2 border-[#F7813F] hover:bg-[#F7813F] hover:text-white py-2 px-3 sm:px-4 rounded-full flex items-center font-apex text-xs gap-1 sm:gap-2 group"
+      className="w-fit bg-[#0A0807] border-2 border-[#F7813F] hover:bg-[#F7813F] hover:text-white py-2 px-3 sm:px-4 rounded-full flex items-center font-apex gap-1 sm:gap-2 group"
     >
-      <span className="hidden sm:inline text-main">{text}</span>
+      <span className="text-main text-sm max-sm:text-xs tracking-wider">{text}</span>
       <BsArrowUpRightCircle className="text-base text-[#F7813F] group-hover:text-white" />
     </a>
   )
@@ -59,7 +59,24 @@ const Link = ({link, text})=>{
 
 Link.propTypes = {
   link: PropTypes.string,
-  text: PropTypes.string
+  text: PropTypes.string,
+}
+
+const SeeMore = ({link})=>{
+  
+  return (
+    <a
+      href={link}
+      className="w-fit bg-[#0A0807] border-2 border-[#F7813F] hover:bg-[#F7813F] hover:text-white py-2 px-3 sm:px-4 rounded-full flex items-center font-apex gap-1 sm:gap-2 group"
+    >
+      <span className="text-main text-base max-sm:text-sm tracking-wider">{"See More"}</span>
+      <BsArrowUpRightCircle className="text-base text-[#F7813F] group-hover:text-white" />
+    </a>
+  )
+}
+
+SeeMore.propTypes = {
+  link: PropTypes.string,
 }
  
 export default Events;
