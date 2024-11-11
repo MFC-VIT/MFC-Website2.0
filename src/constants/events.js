683abdd13179b18fa368ec267f5ef58451d2d42e
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2024-11-12 00:30:41 +0530

    chores: add more events; ui: fix explore-button position

--- src/constants/events.js
+++ src/constants/events.js
@@ -1,30 +1,87 @@
 export const events = [
   {
     title: "TechWars",
-    description: "Compete in a thrilling campus-wide WAR, where your team’s strategy is the key to victory. Solve coding questions and conquer territories, play mini-games that offer offline power-ups, sharpening your skills while keeping the fun factor high.",
+    description: "Compete in a thrilling campus-wide WAR, where your team's strategy is the key to victory. Solve coding questions and conquer territories, play mini-games that offer offline power-ups, sharpening your skills while keeping the fun factor high.",
     doe: "28/09/24",
     image: "/src/assets/images/techwars.webp",
     link: "https://github.com/MFC-VIT/TechWars-Frontend"
+  },  
+
+  {
+    title: "Code to Survive",
+    description: "Step into a world where code meets crime, and your wits are your only weapon. Code To Survive blends the thrill of an escape room, the intrigue of a murder mystery, and the strategy of a high-stakes criminal hunt—all in one three round immersive event.",
+    doe: "27/09/24",
+    image: "",
+    link: "https://github.com/MFC-VIT/CTS-BE-24"
+  },  
+
+  {
+    title: "Scavenger of the Year",
+    description: "Embark on an exhilarating scavenger hunt filled with thrilling tasks and challenges! A handpicked list of objectives and riddles will propel your team into a race against time. Dive into side games to unlock exclusive powers that might tip the scales in your favor!",
+    doe: "03/03/24",
+    image: "",
+    link: "https://github.com/MFC-VIT/SOTY-Backend-24"
+  },  
+
+  {
+    title: "InnovationX",
+    description: "The 36-hour hackathon by Mozilla Firefox Club and Advertisement and Marketing Club featured intense ideation, mentorship sessions, and project work, concluding with presentations from the top five teams and a celebratory closing.",
+    doe: "18/03/24",
+    image: "",
+    link: "https://github.com/MFC-VIT/InnovationX-24"
+  },  
+
+  {
+    title: "Cyber Security and Ethical Hacking",
+    description: "The dynamic one-of-its-kind speaker session, featuring Mrs. Pooja V Joshi from EC-Council, focused on enhancing participants' understanding of privacy and cybersecurity.",
+    doe: "13/03/24",
+    image: "",
+    link: "https://www.instagram.com/mfc_vit/"
+  },
+
+  {
+    title: "CodeCraft",
+    description: "This interactive workshop is designed to introduce participants to the fundamentals of JavaScript and the Document Object Model (DOM). This event is tailored for beginners who want to enhance their web development skills by gaining a solid understanding of these core technologies.",
+    doe: "07/03/24",
+    image: "",
+    link: "https://www.instagram.com/mfc_vit/"
+  },
+
+  {
+    title: "CloudForage",
+    description: "CloudForage, led by cloud technology expert Mr. Balamurugan G, was an engaging offline session on AWS Cloud Computing. Concepts covered included AWS fundamentals, advanced cloud architecture, and best practices for building scalable, secure, and innovative projects—perfect for those looking to deepen their cloud expertise and stay ahead in tech!",
+    doe: "13/08/24",
+    image: "",
+    link: "https://www.instagram.com/mfc_vit/"
+  },
+
+  {
+    title: "WebWise",
+    description: "Mozilla Firefox Club VIT Vellore brought web development skills to young minds at Williams School! In an interactive session, students explored website creation using HTML, CSS, and JavaScript. They also learned essential tips for securing their sites against common online threats, empowering them to build and protect their digital spaces.",
+    doe: "09/11/24",
+    image: "",
+    link: "https://www.instagram.com/mfc_vit/"
+  },
+
+  {
+    title: "Queue-rious Coders",
+    description: "Queue up for Queue-rious Coders! This dynamic coding event invites participants of all levels to dive into the complexities of stack and queue data structures. Tackle real-world challenges designed to test and enhance your understanding of these essentials, boost your algorithmic skills, and compete for coding mastery!",
+    doe: "24/10/24",
+    image: "",
+    link: "https://www.instagram.com/mfc_vit/"
   },
   {
-    title: "TechWars",
-    description: "Compete in a thrilling campus-wide WAR, where your team’s strategy is the key to victory. Solve coding questions and conquer territories, play mini-games that offer offline power-ups, sharpening your skills while keeping the fun factor high.",
-    doe: "28/09/24",
-    image: "/src/assets/images/techwars.webp",
-    link: "https://github.com/MFC-VIT/TechWars-Frontend"
+    title : "Arena.exe",
+    description: "Step into Arena.exe! Mozilla Firefox Club presents a thrilling event where participants dive into a game portal packed with challenges. Compete in strategic and skill-based games, connect with fellow gamers, and fight your way to the top!",
+    doe: "08/11/24",
+    image: "",
+    link: "https://www.instagram.com/mfc_vit/"
   },
   {
-    title: "TechWars",
-    description: "Compete in a thrilling campus-wide WAR, where your team’s strategy is the key to victory. Solve coding questions and conquer territories, play mini-games that offer offline power-ups, sharpening your skills while keeping the fun factor high.",
-    doe: "28/09/24",
-    image: "/src/assets/images/techwars.webp",
-    link: "https://github.com/MFC-VIT/TechWars-Frontend"
-  },
-  {
-    title: "TechWars",
-    description: "Compete in a thrilling campus-wide WAR, where your team’s strategy is the key to victory. Solve coding questions and conquer territories, play mini-games that offer offline power-ups, sharpening your skills while keeping the fun factor high.",
-    doe: "28/09/24",
-    image: "/src/assets/images/techwars.webp",
-    link: "https://github.com/MFC-VIT/TechWars-Frontend"
+    title : "WebWhiz",
+    description: "This event by Mozilla Firefox Club brought students together in a hands-on session on HTML and CSS, where participants dove into the essentials of web development by creating their own portfolio websites. This interactive workshop aimed to boost practical skills and inspire future web projects!",
+    doe: "18/04/24",
+    image: "",
+    link: "https://www.instagram.com/mfc_vit/"
   }
 ]
--- src/sections/Event.jsx
+++ src/sections/Event.jsx
@@ -1,83 +1,102 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 import { BsArrowUpRightCircle } from "react-icons/bs";
 import { events } from "../constants/events";
 import { links } from "../constants";
 
+const truncate = (text, length) => {
+  return text.length > length ? text.substring(0, length) + "..." : text;
+};
+
 const Events = () => {
-  return (  
+  return (
     <div className="flex flex-col items-center" id="events">
-      <div className="text-main text-6xl max-md:text-5xl max-sm:text-4xl font-apex">Events at MFC</div>
+      <div className="text-main text-6xl max-md:text-5xl max-sm:text-4xl font-apex">
+        Events at MFC
+      </div>
       <div className="grid grid-cols-2 grid-rows-2 gap-10 max-md:gap-6 p-10 max-md:p-5 max-sm:px-3 max-lg:flex max-lg:flex-col">
-        {events.slice(0, 4).map((event, index)=>{
+        {events.slice(0, 4).map((event, index) => {
           return (
-            <Event {...events[index]} key={index} link={events[index].link || '#events'} />
-          )
+            <Event
+              {...events[index]}
+              key={index}
+              link={events[index].link || "#events"}
+            />
+          );
         })}
       </div>
       <SeeMore link={links.instagram} />
     </div>
   );
-}
+};
 
-const Event = ({
-  title, description, doe, image, link
-}) => {
+const Event = ({ title, description, doe, image, link }) => {
   return (
-    <div className="flex max-sm:flex-col justify-between border-[3px] border-primary rounded-2xl p-4 gap-5 max-sm:gap-1 bg-black">
-      <div className="flex justify-center items-center"><img src={image} className="min-h-36 min-w-36 max-sm:max-h-14 max-sm:max-w-14" /></div>
-      <div className="text-white gap-1 max-sm:gap-0 flex flex-col">
-        <div className="font-apex mb-1 tracking-wider text-primary font-bold text-3xl max-md:text-2xl">{title}</div>
-        <div className="font-yoshiro text-main text-lg max-md:text-base tracking-wide text-ellipsis mb-1">{description}</div>
-        <div className="font-yoshiro text-orange-500">{doe}</div>
-        <div className="w-full flex justify-end">
-        <Link link={link} text="Explore" textClass={"text-lg"} />
+    <div className="flex max-sm:flex-col justify-between border-[3px] border-primary rounded-2xl p-4 min-h-64 gap-5 max-sm:gap-1 bg-black">
+      <div className="flex justify-center items-center">
+        <img
+          src={image}
+          className="min-h-36 min-w-36 max-sm:max-h-14 max-sm:max-w-14"
+        />
+      </div>
+      <div className="text-white gap-1 max-sm:gap-0 flex flex-col relative">
+        <div className="font-apex mb-1 tracking-wider text-primary font-bold text-3xl max-md:text-2xl">
+          {title}
+        </div>
+        <div className="font-yoshiro text-main text-lg max-md:text-base tracking-wide text-ellipsis">
+          {truncate(description, 200)}
+        </div>
+        <div className="font-yoshiro text-orange-500 mb-3">{doe}</div>
+        <div className="w-full flex justify-end absolute bottom-0">
+          <Link link={link} text="Explore" textClass={"text-lg"} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 Event.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   doe: PropTypes.string,
   image: PropTypes.string,
-  link: PropTypes.string
-}
+  link: PropTypes.string,
+};
 
-const Link = ({link, text})=>{
-  
+const Link = ({ link, text }) => {
   return (
     <a
       href={link}
       className="w-fit bg-[#0A0807] border-2 border-[#F7813F] hover:bg-[#F7813F] hover:text-white py-2 px-3 sm:px-4 rounded-full flex items-center font-apex gap-1 sm:gap-2 group"
     >
-      <span className="text-main text-sm max-sm:text-xs tracking-wider">{text}</span>
+      <span className="text-main text-sm max-sm:text-xs tracking-wider">
+        {text}
+      </span>
       <BsArrowUpRightCircle className="text-base text-[#F7813F] group-hover:text-white" />
     </a>
-  )
-}
+  );
+};
 
 Link.propTypes = {
   link: PropTypes.string,
   text: PropTypes.string,
-}
+};
 
-const SeeMore = ({link})=>{
-  
+const SeeMore = ({ link }) => {
   return (
     <a
       href={link}
       className="w-fit bg-[#0A0807] border-2 border-[#F7813F] hover:bg-[#F7813F] hover:text-white py-2 px-3 sm:px-4 rounded-full flex items-center font-apex gap-1 sm:gap-2 group"
     >
-      <span className="text-main text-base max-sm:text-sm tracking-wider">{"See More"}</span>
+      <span className="text-main text-base max-sm:text-sm tracking-wider">
+        {"See More"}
+      </span>
       <BsArrowUpRightCircle className="text-base text-[#F7813F] group-hover:text-white" />
     </a>
-  )
-}
+  );
+};
 
 SeeMore.propTypes = {
   link: PropTypes.string,
-}
- 
+};
+
 export default Events;
