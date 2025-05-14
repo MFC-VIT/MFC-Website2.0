1f6f354edaa724bccaa92f9f25fe70ad20c575f8
Abhinav Garg <abhichhrp@gmail.com>
2025-05-14 21:47:50 +0530

    fix: blog section

--- src/App.jsx
+++ src/App.jsx
@@ -237,7 +237,7 @@ function App() {
               >
                 <AboutUs />
               </motion.section>
-              
+                 
               <motion.section
                 initial="hidden"
                 whileInView="visible"
